fd29200e59360fe53d3345c4f499ec4aa4f0f673
Josh <joshmvince@gmail.com>
2017-05-20 12:04:19 +0100

    css and url loaders for webpack and add react to the index file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
   entry: './client/index.js',
@@ -14,9 +16,38 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use:
+            combineLoaders([{
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              }
+            }])
+        })
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'url-loader'
+      }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new ExtractTextPlugin('styles.css')
+    ]
 }
