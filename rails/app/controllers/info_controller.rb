adc3d9161c787e9eaa189613afebd69d8a5a5655
Josh Vince <joshmvince@gmail.com>
2022-10-20 17:30:12 +0100

    Add me page

---
+++ rails/app/controllers/info_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class InfoController < ApplicationController
+end
--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -2,5 +2,12 @@
 
 # rubocop:disable Style/Documentation
 module ApplicationHelper
+  def underlined_external_link(text, url, options = {})
+    link_to(text, url, class: 'underline', target: :_blank, rel: :noopener, **options)
+  end
+
+  def underlined_internal_link(text, url, options = {})
+    link_to(text, url, class: 'underline', **options)
+  end
 end
 # rubocop:enable Style/Documentation
