2ab11ba5b09000938700cbab8417f85e964ca421
Josh Vince <joshmvince@gmail.com>
2022-10-21 11:54:37 +0100

    Sidebar menu

--- rails/app/helpers/application_helper.rb
+++ rails/app/helpers/application_helper.rb
@@ -2,12 +2,5 @@
 
 # rubocop:disable Style/Documentation
 module ApplicationHelper
-  def underlined_external_link(text, url, options = {})
-    link_to(text, url, class: 'underline', target: :_blank, rel: :noopener, **options)
-  end
-
-  def underlined_internal_link(text, url, options = {})
-    link_to(text, url, class: 'underline', **options)
-  end
 end
 # rubocop:enable Style/Documentation
---
+++ rails/app/helpers/info_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
+module InfoHelper
+  def underlined_external_link(text, url, options = {})
+    link_to(text, url, class: 'underline', target: :_blank, rel: :noopener, **options)
+  end
+
+  def underlined_internal_link(text, url, options = {})
+    link_to(text, url, class: 'underline', **options)
+  end
+
+  def sidebar_link(text, url, options = {})
+    link_to(text, url, class: 'mb-1 hover:underline', **options)
+  end
+end
+# rubocop:enable Style/Documentation
