5be2b037799a6b009b63b7af429ebb5f4b86f4f3
Josh Vince <joshmvince@gmail.com>
2023-01-21 20:26:40 +0000

    Remove old JS app

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders my name', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/josh/i);
-  expect(linkElement).toBeInTheDocument();
-});
