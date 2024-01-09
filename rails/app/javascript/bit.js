f6a92661daeeb4cb0ed022760e65a7bf69a98800
Josh Vince <joshmvince@gmail.com>
2024-01-09 15:00:32 +0000

    Draft 5

--- rails/app/javascript/bit.js
+++ rails/app/javascript/bit.js
@@ -1,28 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("worked")
+  transitionIntoAurora();
+  transitionIntoDawn();
+});
+
+function transitionIntoAurora() {
   let nightSkyParts = document.querySelectorAll(".night-sky div");
   let auroraContainer = document.querySelector(".aurora");
-  let dawnSkyParts = document.querySelectorAll(".dawn-sky div");
-
 
   let observer = new IntersectionObserver(function(entries) {
       entries.forEach(entry => {
           if (entry.isIntersecting) {
             auroraContainer.style.opacity = 1;
             nightSkyParts.forEach(part => { part.style.opacity = 0; })
-            dawnSkyParts.forEach(part => { part.style.opacity = 0; })
           }
           else {
             auroraContainer.style.opacity = 0;
             nightSkyParts.forEach(part => { part.style.opacity = 1; })
-            dawnSkyParts.forEach(part => {
-              part.style.opacity = 1;
-              part.style.background = `linear-gradient(180deg, var(--midnight), var(--dawn))`;
-            })
           }
 
       });
   }, { threshold: 0.2 }); // 1.0 means fully in the viewport
 
   observer.observe(auroraContainer);
-});
+}
+
+function transitionIntoDawn() {
+  let auroraContainer = document.querySelector(".aurora");
+  let dawnSkyContainer = document.querySelector(".dawn-sky");
+  let dawnSkyParts = document.querySelectorAll(".dawn-sky div");
+
+  let observer = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          auroraContainer.style.opacity = 0;
+          dawnSkyParts.forEach(part => {
+            part.style.opacity = 1;
+            part.style.background = `linear-gradient(180deg, var(--midnight), var(--dawn))`;
+          })
+        }
+        else {
+          auroraContainer.style.opacity = 1;
+          dawnSkyParts.forEach(part => { part.style.opacity = 0; })
+        }
+
+    });
+  }, { threshold: 0.3 }); // 1.0 means fully in the viewport
+
+  observer.observe(dawnSkyContainer);
+}
