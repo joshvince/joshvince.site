b047eb2e4307dc4f365b3c65fc34c60a4185748d
Josh Vince <joshmvince@gmail.com>
2024-01-11 11:08:54 +0000

    more tweaks

--- rails/app/javascript/bit.js
+++ rails/app/javascript/bit.js
@@ -1,9 +1,12 @@
+window.hasSeenDawn = false;
+
 document.addEventListener("DOMContentLoaded", function() {
-  transitionIntoAurora();
-  transitionIntoDawn();
+  handleAuroraTransitions();
+  handleDawnTransitions();
+  animateClocks();
 });
 
-function transitionIntoAurora() {
+function handleAuroraTransitions() {
   let nightSkyParts = document.querySelectorAll(".night-sky div");
   let auroraContainer = document.querySelector(".aurora");
 
@@ -11,20 +14,24 @@ function transitionIntoAurora() {
       entries.forEach(entry => {
           if (entry.isIntersecting) {
             auroraContainer.style.opacity = 1;
+            auroraContainer.classList.add('animate-aurora')
             nightSkyParts.forEach(part => { part.style.opacity = 0; })
           }
           else {
             auroraContainer.style.opacity = 0;
+            setTimeout(() => {
+              auroraContainer.classList.remove('animate-aurora')
+            }, 2000);
             nightSkyParts.forEach(part => { part.style.opacity = 1; })
           }
 
       });
-  }, { threshold: 0.2 }); // 1.0 means fully in the viewport
+  }, { threshold: 0.1 }); // 1.0 means fully in the viewport
 
   observer.observe(auroraContainer);
 }
 
-function transitionIntoDawn() {
+function handleDawnTransitions() {
   let auroraContainer = document.querySelector(".aurora");
   let dawnSkyContainer = document.querySelector(".dawn-sky");
   let dawnSkyParts = document.querySelectorAll(".dawn-sky div");
@@ -33,10 +40,11 @@ function transitionIntoDawn() {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
           auroraContainer.style.opacity = 0;
+          dawnSkyContainer.style.background = `linear-gradient(180deg, var(--midnight), var(--dawn))`;
           dawnSkyParts.forEach(part => {
             part.style.opacity = 1;
-            part.style.background = `linear-gradient(180deg, var(--midnight), var(--dawn))`;
           })
+          window.hasSeenDawn = true;
         }
         else {
           auroraContainer.style.opacity = 1;
@@ -47,4 +55,26 @@ function transitionIntoDawn() {
   }, { threshold: 0.3 }); // 1.0 means fully in the viewport
 
   observer.observe(dawnSkyContainer);
+}
+
+function animateClocks() {
+  const clocks = document.querySelectorAll('.clock')
+
+  clocks.forEach(clock => {
+    let observer = new IntersectionObserver(function(entries) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            clock.querySelector('.minute-hand').classList.add('animate-minute')
+            clock.querySelector('.hour-hand').classList.add('animate-hour')
+          }
+          else {
+            clock.querySelector('.minute-hand').classList.remove('animate-minute')
+            clock.querySelector('.hour-hand').classList.remove('animate-hour')
+          }
+
+      });
+    }, { threshold: 0.1 }); // 1.0 means fully in the viewport
+
+    observer.observe(clock);
+  })
 }
