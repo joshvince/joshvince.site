02e837e1ea8797372afdf55672d3951349f05435
Josh Vince <joshmvince@gmail.com>
2024-01-13 11:43:45 +0000

    New homepage

--- rails/app/controllers/info_controller.rb
+++ rails/app/controllers/info_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class InfoController < ApplicationController
+  def home
+    @page_title = "Josh Vince"
+    @highlighted_blog_posts = BlogPost.order(published_on: :desc).limit(5)
+  end
+
   def me
     @page_title = "About me"
   end
