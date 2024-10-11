48f31caf0448b543e59a23c553cbaaa83f4a6b88
Josh Vince <joshmvince@gmail.com>
2024-10-11 19:16:59 +0100

    add missing host to production

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   config.public_file_server.enabled = true
 
+  routes.default_url_options[:host] = 'joshvince.site'
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
